46cb2230a90b0bba768ffef3519544443529ce19
M.Mahadi <motivation1424@gmail.com>
2023-08-23 11:38:30 +0600

    Fix name edit problem

--- bpi.py
+++ bpi.py
@@ -1,4 +1,3 @@
-import os
 credentials = {'bpi': '123'}
 
 
@@ -7,7 +6,6 @@ def login():
     password = input('Password: ')  # Use getpass to input password securely
     if credentials.get(user) == password:
         return True
-        os.system("clear")
     else:
         print('Wrong username or password')
         return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
-from bpi import login
 
+from bpi import login  # Assuming you have your own login function
 
 # Function to read data from CSV
 def read_from_csv():
@@ -9,11 +9,11 @@ def read_from_csv():
         with open('student_data.csv', 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                name = row['Name']
-                department = row['Department']
                 roll = row['Roll']
+                department = row['Department']
+                name = row['Name']
                 session = row['Session']
-                student_list[name] = {'Department': department, 'Roll': roll, 'Session': session}
+                student_list[roll] = {'Name': name, 'Department': department, 'Session': session}
         return student_list
     except FileNotFoundError:
         return {}  # Return empty dictionary if the file doesn't exist
@@ -21,28 +21,31 @@ def read_from_csv():
 
 # Function to save data to CSV
 def save_to_csv(student_list):
-    with open('student_data.csv', 'w', newline='') as csvfile:
-        fieldnames = ['Name', 'Department', 'Roll', 'Session']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    with open('student_data.csv', 'w', newline='') as file:  # Use 'w' mode to overwrite the file
+        fieldnames = ['Roll', 'Department', 'Name', 'Session']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
 
         writer.writeheader()
-        for name, info in student_list.items():
+        for roll, info in student_list.items():
             writer.writerow(
-                {'Name': name, 'Department': info['Department'], 'Roll': info['Roll'], 'Session': info['Session']})
+                {'Roll': roll, 'Name': info['Name'], 'Department': info['Department'], 'Session': info['Session']})
 
 
 def show_student(student_list):
     if not student_list:
         print('No students are listed')
     else:
-        print('Name\t\tDepartment\t\tRoll\t\tSession')
-        for studentn, info in student_list.items():
-            print("{}\t\t{}\t\t{}\t\t{}".format(studentn, info['Department'], info['Roll'], info['Session']))
+        print('Name\t\tRoll\t\tDepartment\t\tSession')
+        for roll, info in student_list.items():
+            print("{}\t\t{}\t\t{}\t\t\t{}".format(info['Name'],roll,  info['Department'], info['Session']))
 
 
 # Main program
 def main():
     student_list = read_from_csv()
+
+
+
 
     if login():
         while True:
@@ -52,7 +55,6 @@ def main():
  \t\t|   | |       |  
  \t\to--o  o     o-O-o
 Barisal Polytechnic Institute                
-                  
 
             ''')
             choice = int(input('1. Add Student\n'
@@ -61,55 +63,52 @@ Barisal Polytechnic Institute
                                '4. Remove Student\n'
                                '5. Search Student\n'
                                '6. Exit\n'
-                               'Enter your choice It Should Be Number 1-6: '))
+                               'Enter your choice (1-6): '))
 
             if choice == 1:
                 name = input('Input Student Name:')
                 dep = input('Input Student Department:')
                 roll = input('Input Student Roll:')
                 session = input('Input Student Session:')
-                student_list[name] = {'Department': dep, 'Roll': roll, 'Session': session}
+                student_list[roll] = {'Name': name, 'Department': dep, 'Session': session}
                 save_to_csv(student_list)
 
             elif choice == 2:
-                if not student_list:
-                    print('Student info is empty.')
-                else:
-                    show_student(student_list)
+                show_student(student_list)
 
             elif choice == 3:
-                edit_name = input("Which student's data:")
-                if edit_name in student_list:
+                edit_roll = input("Which student's Roll to edit:")
+                if edit_roll in student_list:
+                    name = input('Input Student Name:')
                     dep = input('Input Student Department:')
-                    roll = input('Input Student Roll:')
                     session = input('Input Student Session:')
-                    student_list[edit_name] = {'Department': dep, 'Roll': roll, 'Session': session}
+                    student_list[edit_roll] = {'Name': name, 'Department': dep, 'Session': session}
                     save_to_csv(student_list)
                 else:
-                    print('Incorrect Student Name!')
+                    print('Student Roll not found!')
 
             elif choice == 4:
-                rmst = input("Which student's data:")
-                if rmst in student_list:
-                    ask = input("Do you want to Remove This Student Y/N?")
-                    if ask == 'Y' or ask == 'y':
-                        student_list.pop(rmst)
+                remove_roll = input("Which student's Roll to remove:")
+                if remove_roll in student_list:
+                    ask = input("Do you want to remove this student? (Y/N): ")
+                    if ask.lower() == 'y':
+                        student_list.pop(remove_roll)
                         print('Successfully removed the student!')
                         save_to_csv(student_list)
                     else:
-                        print('Incorrect Input!')
+                        print('No changes were made.')
                 else:
-                    print('Incorrect Student Name!')
+                    print('Student Roll not found!')
 
             elif choice == 5:
-                src = input('Search Student:')
-                if src in student_list:
-                    print(student_list[src])
+                search_roll = input('Enter Student Roll to search:')
+                if search_roll in student_list:
+                    print(student_list[search_roll])
                 else:
-                    print('Student Not Found')
+                    print('Student not found.')
 
             elif choice == 6:
-                con = input('')
+                print('Exiting the program.')
                 break
             else:
                 print('Invalid choice. Please choose a number between 1 and 6.')
